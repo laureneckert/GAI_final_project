fd6fa21929cbb98878813d1bec935f910fcb4473
Lauren Eckert <laureneckert5@gmail.com>
2023-12-01 17:50:06 -0500

    z

--- MLmodel.py
+++ MLmodel.py
@@ -11,67 +11,154 @@ Model: CycleGAN
 # MLmodel.py
 
 import tensorflow as tf
-from tensorflow.keras import Model
-from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose, Activation, LeakyReLU, Concatenate
-from tensorflow.keras.optimizers import Adam
-import os
+from keras import Model
+from keras.layers import Input, Conv2D, Conv2DTranspose, Activation, LeakyReLU, Add, BatchNormalization, UpSampling2D
+from keras.optimizers import Adam
+from keras.losses import BinaryCrossentropy, MeanAbsoluteError
 
-def build_generator():
+def resnet_block(input_layer, filters, kernel_size=(3, 3)):
     """
-    Build the generator model for the CycleGAN.
-    This function should return a Keras Model.
+    Create a ResNet block with two convolutional layers and a skip connection.
+
+    Parameters:
+    input_layer (Tensor): Input tensor to the ResNet block.
+    filters (int): Number of filters in the convolutional layers.
+    kernel_size (tuple): Size of the kernel for the convolutional layers.
+
+    Returns:
+    Tensor: Output tensor of the ResNet block.
     """
-    # Sample generator structure - modify as per requirements
-    inputs = Input(shape=(256, 256, 3))
+    x = Conv2D(filters, kernel_size, padding='same')(input_layer)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
 
-    # Downsample
-    x = Conv2D(64, (4, 4), strides=2, padding='same')(inputs)
-    x = LeakyReLU(alpha=0.2)(x)
+    x = Conv2D(filters, kernel_size, padding='same')(x)
+    x = BatchNormalization()(x)
 
-    # Upsample
-    x = Conv2DTranspose(64, (4, 4), strides=2, padding='same')(x)
+    # Skip Connection
+    x = Add()([x, input_layer])
+    return Activation('relu')(x)
+
+def build_generator(input_shape=(256, 256, 3), num_resnet_blocks=9):
+    """
+    Build a generator model using a ResNet-based architecture.
+
+    Parameters:
+    input_shape (tuple): Shape of the input image.
+    num_resnet_blocks (int): Number of ResNet blocks.
+
+    Returns:
+    Model: Generator model.
+    """
+    inputs = Input(shape=input_shape)
+
+    # Initial Convolution Block
+    x = Conv2D(64, (7, 7), padding='same')(inputs)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # Downsampling
+    x = Conv2D(128, (3, 3), strides=2, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(256, (3, 3), strides=2, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # ResNet Blocks
+    for _ in range(num_resnet_blocks):
+        x = resnet_block(x, 256)
+
+    # Upsampling
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2D(128, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2D(64, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # Output Convolution
+    x = Conv2D(3, (7, 7), padding='same')(x)
     x = Activation('tanh')(x)
 
     return Model(inputs, x, name='Generator')
 
-def build_discriminator():
+def build_discriminator(input_shape=(256, 256, 3)):
     """
-    Build the discriminator model for the CycleGAN.
-    This function should return a Keras Model.
-    """
-    # Sample discriminator structure - modify as per requirements
-    inputs = Input(shape=(256, 256, 3))
+    Build a discriminator model using a PatchGAN architecture.
 
+    Parameters:
+    input_shape (tuple): Shape of the input image.
+
+    Returns:
+    Model: Discriminator model.
+    """
+    inputs = Input(shape=input_shape)
+
+    # Convolutional layers with increasing filters
     x = Conv2D(64, (4, 4), strides=2, padding='same')(inputs)
     x = LeakyReLU(alpha=0.2)(x)
+
     x = Conv2D(128, (4, 4), strides=2, padding='same')(x)
-    x = Activation('sigmoid')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    x = Conv2D(256, (4, 4), strides=2, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    x = Conv2D(512, (4, 4), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    # Output convolution layer
+    x = Conv2D(1, (4, 4), padding='same')(x)
 
     return Model(inputs, x, name='Discriminator')
 
 def compile_models(generator, discriminator, lr=0.0002, beta_1=0.5):
     """
-    Compile the CycleGAN models.
-    """
-    # Compile the generator
-    generator.compile(loss='binary_crossentropy', optimizer=Adam(lr=lr, beta_1=beta_1))
+    Compile the generator and discriminator models with specified optimizers and loss functions.
 
-    # For the discriminator, freeze generator's layers
-    discriminator.compile(loss='binary_crossentropy', optimizer=Adam(lr=lr, beta_1=beta_1))
+    Parameters:
+    generator (Model): Generator model to be compiled.
+    discriminator (Model): Discriminator model to be compiled.
+    lr (float): Learning rate for the Adam optimizer.
+    beta_1 (float): Beta1 parameter for the Adam optimizer.
+    """
+    generator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=lr, beta_1=beta_1))
+    discriminator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=lr, beta_1=beta_1))
 
 def train_model(art_images, photo_images, generator_AtoB, generator_BtoA, discriminator_A, discriminator_B, epochs, batch_size):
-    # Define loss functions
+    """
+    Train the CycleGAN models on the provided datasets.
+
+    Parameters:
+    art_images (Iterator): Batched images from the ArtBench-10 dataset.
+    photo_images (Iterator): Batched images from the ImageNet dataset.
+    generator_AtoB (Model): Generator model to translate from domain A to B.
+    generator_BtoA (Model): Generator model to translate from domain B to A.
+    discriminator_A (Model): Discriminator model for domain A.
+    discriminator_B (Model): Discriminator model for domain B.
+    epochs (int): Number of epochs for training.
+    batch_size (int): Size of the image batches.
+    """
+    # Loss functions
     adversarial_loss = BinaryCrossentropy(from_logits=True)
     cycle_loss = MeanAbsoluteError()
     identity_loss = MeanAbsoluteError()
 
-    # Define the optimizers
+    # Optimizers
     gen_optimizer = Adam(learning_rate=0.0002, beta_1=0.5)
     disc_optimizer = Adam(learning_rate=0.0002, beta_1=0.5)
 
+    # Training loop
     for epoch in range(epochs):
-        for i in range(min(len(art_images), len(photo_images)) // batch_size):
-            # Sample a batch of images from both domains
+        # Sample a batch of images from both domains
             ...
 
             with tf.GradientTape(persistent=True) as tape:
@@ -102,10 +189,14 @@ def train_model(art_images, photo_images, generator_AtoB, generator_BtoA, discri
 
     return generator_AtoB, generator_BtoA
 
-
 def save_model(model, model_name, save_dir='models'):
     """
-    Save a trained model.
+    Save a trained model to the specified directory.
+
+    Parameters:
+    model (Model): The trained model to be saved.
+    model_name (str): Name for the saved model file.
+    save_dir (str): Directory to save the model.
     """
     if not os.path.isdir(save_dir):
         os.makedirs(save_dir)
@@ -115,7 +206,14 @@ def save_model(model, model_name, save_dir='models'):
 
 def load_model(model_name, model_dir='models'):
     """
-    Load a trained model.
+    Load a trained model from the specified directory.
+
+    Parameters:
+    model_name (str): Name of the model file to be loaded.
+    model_dir (str): Directory from where to load the model.
+
+    Returns:
+    Model: The loaded Keras model.
     """
     model_path = os.path.join(model_dir, f'{model_name}.h5')
     if os.path.exists(model_path):
@@ -123,10 +221,3 @@ def load_model(model_name, model_dir='models'):
     else:
         print(f"No model found at {model_path}")
         return None
-
-# Example usage:
-# gen = build_generator()
-# disc = build_discriminator()
-# compile_models(gen, disc)
-# train_model(art_images, photo_images, gen, disc, epochs=10, batch_size=1)
-# save_model(gen, 'art_style_generator')
--- driverFunctions.py
+++ driverFunctions.py
@@ -9,20 +9,40 @@ Databases: ArtBench-10, ImageNet
 Model: CycleGAN
 
 """
-#driverFunctions.py
+# driverFunctions.py
 
 import numpy as np
 import matplotlib.pyplot as plt
-from keras.preprocessing.image import img_to_array, load_img
+from keras.preprocessing.image import img_to_array, load_img, ImageDataGenerator
 import cv2
+import os
 
 def load_and_preprocess_image(image_path, target_size=(256, 256)):
+    """
+    Load and preprocess an image from the specified path.
+
+    Parameters:
+    image_path (str): Path to the image file.
+    target_size (tuple): Desired size for the image as (width, height).
+
+    Returns:
+    numpy.ndarray: Preprocessed image array.
+    """
     image = load_img(image_path, target_size=target_size)
     image = img_to_array(image)
     image = (image - 127.5) / 127.5  # Normalize the image
     return np.expand_dims(image, axis=0)
 
 def postprocess_image(image_tensor):
+    """
+    Postprocess the image tensor to convert it into a displayable format.
+
+    Parameters:
+    image_tensor (numpy.ndarray): The image tensor output from the model.
+
+    Returns:
+    numpy.ndarray: Postprocessed image suitable for display or saving.
+    """
     image_tensor = (image_tensor * 127.5) + 127.5
     image_tensor = np.array(image_tensor, dtype=np.uint8)
     if np.ndim(image_tensor) > 3:
@@ -30,14 +50,70 @@ def postprocess_image(image_tensor):
     return image_tensor
 
 def apply_art_style(image_path, generator):
+    """
+    Apply the art style transformation to an image using the provided generator.
+
+    Parameters:
+    image_path (str): Path to the input image.
+    generator (tf.keras.Model): The generator model for style transfer.
+
+    Returns:
+    numpy.ndarray: The image with the art style applied.
+    """
     input_image = load_and_preprocess_image(image_path)
     generated_image = generator.predict(input_image)
     return postprocess_image(generated_image)
 
-def save_or_display_image(image, save=False, display=False, save_path='styled_image.jpg'):
+def save_or_display_image(image, save=False, display=True, save_path='styled_image.jpg'):
+    """
+    Save or display the processed image.
+
+    Parameters:
+    image (numpy.ndarray): The image to be saved or displayed.
+    save (bool): Whether to save the image to disk.
+    display (bool): Whether to display the image.
+    save_path (str): Path to save the image if save is True.
+    """
     if save:
         cv2.imwrite(save_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
     if display:
         plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         plt.axis('off')
         plt.show()
+
+def image_generator(dataset_path, batch_size, target_size=(256, 256)):
+    """
+    Create a generator that yields batches of images from a dataset directory.
+
+    Parameters:
+    dataset_path (str): Path to the dataset directory.
+    batch_size (int): Number of images per batch.
+    target_size (tuple): Desired size for each image as (width, height).
+
+    Returns:
+    Iterator: A generator that yields image batches.
+    """
+    datagen = ImageDataGenerator(rescale=1./127.5, preprocessing_function=lambda x: x - 1.0)
+    return datagen.flow_from_directory(
+        dataset_path,
+        target_size=target_size,
+        batch_size=batch_size,
+        class_mode=None)  # 'None' means no labels are returned
+
+def load_datasets(artbench_path, imagenet_path, batch_size, target_size=(256, 256)):
+    """
+    Create generators for the ArtBench-10 and ImageNet datasets.
+
+    Parameters:
+    artbench_path (str): Path to the ArtBench-10 dataset directory.
+    imagenet_path (str): Path to the ImageNet dataset directory.
+    batch_size (int): Number of images per batch.
+    target_size (tuple): Desired size for each image as (width, height).
+
+    Returns:
+    Tuple[Iterator, Iterator]: Tuple of generators for both datasets.
+    """
+    art_images_generator = image_generator(artbench_path, batch_size, target_size)
+    photo_images_generator = image_generator(imagenet_path, batch_size, target_size)
+
+    return art_images_generator, photo_images_generator
