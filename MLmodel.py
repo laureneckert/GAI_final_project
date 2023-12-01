ed89d199b151d27f1dc39939ccb350b17ec0fe45
Lauren Eckert <laureneckert5@gmail.com>
2023-12-01 15:09:34 -0500

    i love school

--- MLmodel.py
+++ MLmodel.py
@@ -7,32 +7,92 @@ Final Project
 Project topic: transitive learning - applying art historical styles to personal photos
 Databases: ArtBench-10, ImageNet
 Model: CycleGAN
-
-MLmodel.py
 """
+
+# MLmodel.py
+import tensorflow as tf
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Input, Conv2D, Activation
+from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose, Activation, LeakyReLU, Concatenate
+from tensorflow.keras.optimizers import Adam
+import os
 
 def build_generator():
-    # Define the generator model
-    # ...
+    """
+    Build the generator model for the CycleGAN.
+    This function should return a Keras Model.
+    """
+    # Sample generator structure - modify as per requirements
+    inputs = Input(shape=(256, 256, 3))
+
+    # Downsample
+    x = Conv2D(64, (4, 4), strides=2, padding='same')(inputs)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    # Upsample
+    x = Conv2DTranspose(64, (4, 4), strides=2, padding='same')(x)
+    x = Activation('tanh')(x)
+
+    return Model(inputs, x, name='Generator')
 
 def build_discriminator():
-    # Define the discriminator model
-    # ...
-
-def load_trained_model(model_path):
-    # Load the trained model (modify this according to your model saving/loading method)
-    # ...
-    return model
-
-def train_model(art_images, photo_images):
-    # Implement the training loop
-    # ...
-
-# Example model building (simplified)
-def example_model():
+    """
+    Build the discriminator model for the CycleGAN.
+    This function should return a Keras Model.
+    """
+    # Sample discriminator structure - modify as per requirements
     inputs = Input(shape=(256, 256, 3))
-    x = Conv2D(64, (3, 3), padding='same')(inputs)
-    x = Activation('relu')(x)
-    return Model(inputs, x)
+
+    x = Conv2D(64, (4, 4), strides=2, padding='same')(inputs)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = Conv2D(128, (4, 4), strides=2, padding='same')(x)
+    x = Activation('sigmoid')(x)
+
+    return Model(inputs, x, name='Discriminator')
+
+def compile_models(generator, discriminator, lr=0.0002, beta_1=0.5):
+    """
+    Compile the CycleGAN models.
+    """
+    # Compile the generator
+    generator.compile(loss='binary_crossentropy', optimizer=Adam(lr=lr, beta_1=beta_1))
+
+    # For the discriminator, freeze generator's layers
+    discriminator.compile(loss='binary_crossentropy', optimizer=Adam(lr=lr, beta_1=beta_1))
+
+def train_model(art_images, photo_images, generator, discriminator, epochs, batch_size):
+    """
+    Train the CycleGAN model.
+    art_images: Art images from ArtBench-10 dataset
+    photo_images: Photograph images from ImageNet dataset
+    """
+    for epoch in range(epochs):
+        # Implement the training logic here
+        pass
+
+def save_model(model, model_name, save_dir='models'):
+    """
+    Save a trained model.
+    """
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+    model_path = os.path.join(save_dir, f'{model_name}.h5')
+    model.save(model_path)
+    print(f"Model saved at {model_path}")
+
+def load_model(model_name, model_dir='models'):
+    """
+    Load a trained model.
+    """
+    model_path = os.path.join(model_dir, f'{model_name}.h5')
+    if os.path.exists(model_path):
+        return tf.keras.models.load_model(model_path)
+    else:
+        print(f"No model found at {model_path}")
+        return None
+
+# Example usage:
+# gen = build_generator()
+# disc = build_discriminator()
+# compile_models(gen, disc)
+# train_model(art_images, photo_images, gen, disc, epochs=10, batch_size=1)
+# save_model(gen, 'art_style_generator')
--- driver.py
+++ driver.py
@@ -15,17 +15,32 @@ import driverFunctions as df
 import MLmodel
 
 def main():
-    # Load trained CycleGAN model (modify with actual model loading code)
-    generator = MLmodel.load_trained_model('path/to/trained_model')
+    # Define parameters
+    epochs = 10  # Number of training epochs (adjust as needed)
+    batch_size = 1  # Batch size for training (adjust as needed)
+    model_save_name = 'art_style_generator'  # Name to save your trained generator model
 
-    # Input from user
+    # Load or train your model
+    # Uncomment the following lines if you need to train the model
+    # art_images, photo_images = df.load_datasets()  # Implement this function in driverFunctions.py
+    # generator, discriminator = MLmodel.build_generator(), MLmodel.build_discriminator()
+    # MLmodel.compile_models(generator, discriminator)
+    # MLmodel.train_model(art_images, photo_images, generator, discriminator, epochs, batch_size)
+    # MLmodel.save_model(generator, model_save_name)
+
+    # Or load the pre-trained model
+    generator = MLmodel.load_model(model_save_name)
+    if generator is None:
+        print("Model not found. Exiting.")
+        return
+
+    # Input from user for style transfer
     input_image_path = input("Enter the path of your photograph: ")
 
-    # Apply art style
+    # Apply art style using the loaded generator
     styled_image = df.apply_art_style(input_image_path, generator)
 
     # Save or display styled image
     df.save_or_display_image(styled_image, save=True, display=True)
-
 if __name__ == "__main__":
     main()
